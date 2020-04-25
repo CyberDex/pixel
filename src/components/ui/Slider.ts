5eebb2a0ee1ee5fbfaa1bfb298adfec00cda8791
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-25 18:25:04 +0300

    fit(slider): update slider

--- src/components/ui/Slider.ts
+++ src/components/ui/Slider.ts
@@ -4,20 +4,55 @@ import { Graphics } from 'pixi.js'
 export class Slider extends View {
     private background: Graphics
     private slider: Graphics
+    private sliderData: any
+    private sliderDragging: boolean = false
 
     constructor(
-        public x: number = 0,
-        public y: number = 0,
-        public width: number = 200,
-        public height: number = 40,
+        public w: number = 200,
+        public h: number = 40,
         public bgColor: number = 0xDE3249,
         public fgColor: number = 0xffffff,
-
     ) {
         super()
-        this.background = this.addRect(x - width * .5, y - height * .5, width, height, bgColor)
+        this.width = w
+        this.height = h
+
+        this.background = this.addRect(0, 0, w, h, bgColor)
         this.addChild(this.background)
-        this.slider = this.addCircle(x, y, height * .5, fgColor)
+        this.addChild(this.addCircle(0, h / 2, h / 2, bgColor))
+        this.addChild(this.addCircle(w, h / 2, h * .5, bgColor))
+
+        this.slider = this.addCircle(0, h / 2, h / 2, fgColor)
         this.addChild(this.slider)
+        this.slider.interactive = true
+        this.slider.buttonMode = true
+        this.slider
+            .on('pointerdown', event => this.onDragStart(event))
+            .on('pointerup', event => this.onDragEnd())
+            .on('pointerupoutside', event => this.onDragEnd())
+            .on('pointermove', event => this.onDragMove())
+    }
+
+    public onDragStart(event) {
+        this.sliderData = event.data
+        this.sliderDragging = true
+    }
+
+    public onDragEnd() {
+        this.sliderDragging = false
+        this.sliderData = null
+
+    }
+
+    public onDragMove() {
+        if (this.sliderDragging) {
+            const newPosition = this.sliderData.getLocalPosition(this)
+            this.slider.x =
+                newPosition.x > this.w
+                    ? this.w
+                    : newPosition.x < 0
+                        ? 0
+                        : newPosition.x
+        }
     }
 }
