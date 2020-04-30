f825332c0f13c2779ff65cfe272d456e13c9afa9
Dmytro Soldatov <dmytro.soldatov@derivco.ee>
2020-04-30 16:53:39 +0300

    config(project): some additional config

--- test/App.test.ts
+++ test/App.test.ts
@@ -5,11 +5,11 @@ import { App, SceneManager } from '../src/index'
 import { Application } from 'pixi.js'
 let app: App
 
-describe('Test App module', () => {
-	test('Create App', () => {
+describe('App module', () => {
+    test('Create App', () => {
         app = new App()
-		expect(app).toBeDefined()
-		expect(app).toBeInstanceOf(Application)
-		expect(app.scenes).toBeInstanceOf(SceneManager)
-	})
+        expect(app).toBeDefined()
+        expect(app).toBeInstanceOf(Application)
+        expect(app.scenes).toBeInstanceOf(SceneManager)
+    })
 })
