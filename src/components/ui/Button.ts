a3cbef94f2d9e36888ee1a54d21904bd7ed21e45
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-30 22:08:52 +0300

    config(Button): refactor button

--- src/components/ui/Button.ts
+++ src/components/ui/Button.ts
@@ -1,10 +1,12 @@
 import { View } from '../View'
 import { Graphics } from 'pixi.js'
 import { Label } from '../..'
+import { Const } from '../../helpers/const';
 
 export class Button extends View {
+
 	public set active(active: boolean) {
-		this.alpha = active ? 1 : 0.5
+		this.alpha = active ? 1 : .5
 		this.buttonMode = active
 		this._active = active
 	}
@@ -18,26 +20,21 @@ export class Button extends View {
 	private pressed = false
 	private _active = true
 
-	public constructor(
-		public positionX = 50,
-		public positionY = 50,
-		private readonly w = 200,
-		private readonly h = 30,
-		private readonly textString = '',
-		private readonly textStyle: any = {},
-		private readonly radius = 0,
-		private readonly bgColor: number | string = 0xde3249,
-	) {
+	public constructor(private readonly props: IButton = {}) {
 		super()
-		this.width = w
-		this.height = h
 
-		this.background = this.addRect(0, 0, w, h, Number(bgColor), radius)
+		props.width = props.width || 200
+		props.height = props.height || 30
+
+		this.positionX = props.positionX || 50
+		this.positionY = props.positionY || 50
+
+		this.background = this.addRect(0, 0, props.width, props.height, Number(props.color || Const.defaultColor), props.radius || 0)
 		this.addChild(this.background)
-		this.background.x = -w / 2
-		this.background.y = -h / 2
+		this.background.x = -props.width / 2
+		this.background.y = -props.height / 2
 
-		this.addChild(new Label(textString, textStyle, 0, 0))
+		this.addChild(new Label(props.text || '', props.style || { fill: '#ffffff' }, 0, 0))
 
 		this.interactive = true
 		this.buttonMode = true
@@ -56,9 +53,7 @@ export class Button extends View {
 	}
 
 	private onDown() {
-		if (!this._active) {
-			return
-		}
+		if (!this._active) { return }
 		this.pressed = true
 		this.scale.set(0.95)
 	}
@@ -66,14 +61,23 @@ export class Button extends View {
 	private onUp() {
 		this.scale.set(1)
 		this.pressed = false
-		if (!this._active) {
-			return
-		}
-		this.cb.forEach((cb) => cb())
+		if (!this._active) { return }
+		this.cb.forEach(cb => cb())
 	}
 
 	private onMouseOut() {
 		this.scale.set(1)
 		this.pressed = false
 	}
+}
+
+export interface IButton {
+	text?: string
+	positionX?: number
+	positionY?: number
+	width?: number
+	height?: number
+	radius?: number
+	color?: number | string
+	style?: any
 }
