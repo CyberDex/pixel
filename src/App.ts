d93c95f8821fb3c0e0533bda211caa5b8522cbf2
Dmytro Soldatov <cyberdexx@gmail.com>
2020-05-01 14:28:26 +0300

    config(App): add scenes to views

--- src/App.ts
+++ src/App.ts
@@ -4,22 +4,28 @@ import { ViewManager } from './controllers/ViewManager'
 
 /**
  * Convenience class to create a new PIXI application.
+ * 
  * This class automatically creates the renderer, ticker and root container.
+ * 
  * Additional functionality from Pixil is resize [[renderer]] with [[LayoutManager]]
+ * 
  * Also [[ViewManager]] instance will be created and will be available for use with 'App.scenes' construction
+ * 
+ * App.views holds an instance of [[ViewManager]]
+ * 
+ * App.layout holds an instance of [[LayoutManager]]
  *
  * @export
  * @class App
  * @extends {Application}
  */
-
 export class App extends Application {
-	public scenes: ViewManager
+	public views: ViewManager
 	public layout: LayoutManager
 
 	public constructor(options?: object) {
 		super(options)
-		this.scenes = new ViewManager(this)
+		this.views = new ViewManager(this)
 		this.layout = LayoutManager.instance
 		this.layout.onResize((w, h) => this.renderer.resize(w, h))
 	}
