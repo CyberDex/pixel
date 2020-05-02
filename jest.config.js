04157477185fe15fbdaf3964c182092c11e77fb6
Dmytro Soldatov <cyberdexx@gmail.com>
2020-05-02 20:42:07 +0300

    feat(CodeCov): add codeCov

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  "transform": {
-    "^.+\\.(t|j)sx?$": "ts-jest"
-  },
-  "testRegex": "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  "moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"],
-  watchPlugins: ['jest-watch-master']
+	"transform": {
+		"^.+\\.(t|j)sx?$": "ts-jest"
+	},
+	"testRegex": "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+	"moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"],
+	"collectCoverage": true,
+	watchPlugins: ['jest-watch-master']
 }
